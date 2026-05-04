d18094aa1ee4f7dacf6e8009ad75a16b2ee0b2d4
Zainab Salman <zainabsalman.work@gmail.com>
2026-05-04 11:34:08 +0500

    modified:   public/css/srd-public.css 	modified:   public/templates/resume-drop-page.php

--- public/templates/resume-drop-page.php
+++ public/templates/resume-drop-page.php
@@ -14,9 +14,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Submit Your Resume - <?php bloginfo( 'name' ); ?></title>
 	<?php wp_head(); ?>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
 <body class="srd-page-body">
 
